9b49a59821f995d77836c13207c27a695a61f724
mihalichpalich <pavlovtfspb@gmail.com>
2019-08-13 12:19:12 +0300

    Updates

---
+++ precache-manifest.028bc5c2fcd600a1c2f42d311f292ad1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36a2d5da1cfc234a9beb27199f1ffd6",
+    "url": "/react_resizive_grid/index.html"
+  },
+  {
+    "revision": "ad982ac014e93f001775",
+    "url": "/react_resizive_grid/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2b8dc97f3079caac2f27",
+    "url": "/react_resizive_grid/static/js/2.10b0a915.chunk.js"
+  },
+  {
+    "revision": "ad982ac014e93f001775",
+    "url": "/react_resizive_grid/static/js/main.56d7d2d3.chunk.js"
+  },
+  {
+    "revision": "783d786462c72499293f",
+    "url": "/react_resizive_grid/static/js/runtime~main.7f3bb057.js"
+  }
+]);
