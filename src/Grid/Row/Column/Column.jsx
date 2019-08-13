c5f5158f269e52d39e863f4fd18ad651af539057
mihalichpalich <pavlovtfspb@gmail.com>
2019-08-13 12:21:40 +0300

    подключил gh-pages

--- src/Grid/Row/Column/Column.jsx
+++ src/Grid/Row/Column/Column.jsx
@@ -1,67 +1,70 @@
-import React from 'react';
+import React from "react";
 import $ from "jquery";
 
 class Column extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            columnWidth: '50%',
-            columnHeight: '300px'
-        }
-        this.initResize = this.initResize.bind(this);
-        this.resize = this.resize.bind(this);
-        this.stopResize = this.stopResize.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnWidth: "50%",
+      columnHeight: "300px"
+    };
+    this.initResize = this.initResize.bind(this);
+    this.resize = this.resize.bind(this);
+    this.stopResize = this.stopResize.bind(this);
+  }
 
-    initResize() {
-        $(window).bind('mousemove', this.resize);
-        $(window).bind('mouseup', this.stopResize);
-    }
+  initResize() {
+    $(window).bind("mousemove", this.resize);
+    $(window).bind("mouseup", this.stopResize);
+  }
 
-    resize(e) {
-        let newColumnWidth = (e.clientX - $(".column").offset().left);
-        let newColumnHeight = (e.clientY - $(".column").offset().top);
+  resize(e) {
+    let newColumnWidth = e.clientX - $(".column").offset().left;
+    let newColumnHeight = e.clientY - $(".column").offset().top;
 
-        this.setState({
-            columnWidth: newColumnWidth,
-            columnHeight: newColumnHeight
-        });
-    }
+    this.setState({
+      columnWidth: newColumnWidth,
+      columnHeight: newColumnHeight
+    });
+  }
 
-    stopResize() {
-        $(window).unbind('mousemove', this.resize);
-        $(window).unbind('mouseup', this.stopResize);
-    }
+  stopResize() {
+    $(window).unbind("mousemove", this.resize);
+    $(window).unbind("mouseup", this.stopResize);
+  }
 
-    render() {
-        const ColumnStyles = {
-            columnStyle: {
-                boxSizing: 'border-box',
-                position: 'relative',
-                width: this.state.columnWidth,
-                height: this.state.columnHeight,
-                backgroundColor: 'lightblue',
-                border: '1px solid black'
-            },
-            spanStyle: {
-                display: 'inline-block',
-                position: 'absolute',
-                bottom: 0,
-                right: 0,
-                width: '20px',
-                height: '20px',
-                backgroundColor: 'red'
-            }
-        };
+  render() {
+    const ColumnStyles = {
+      columnStyle: {
+        boxSizing: "border-box",
+        position: "relative",
+        width: this.state.columnWidth,
+        height: this.state.columnHeight,
+        backgroundColor: "lightblue",
+        border: "1px solid black"
+      },
+      spanStyle: {
+        display: "inline-block",
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        width: "20px",
+        height: "20px",
+        backgroundColor: "red"
+      }
+    };
 
-        return (
-            <div className="column" style={ColumnStyles.columnStyle}>
-                ширина {this.state.columnWidth} <br/>
-                высота {this.state.columnHeight}
-             	<span style={ColumnStyles.spanStyle} onMouseDown={this.initResize}></span>
-            </div>
-        )
-    }
+    return (
+      <div className="column" style={ColumnStyles.columnStyle}>
+        ширина {this.state.columnWidth} <br />
+        высота {this.state.columnHeight}
+        <span
+          style={ColumnStyles.spanStyle}
+          onMouseDown={this.initResize}
+        ></span>
+      </div>
+    );
+  }
 }
 
 export default Column;
